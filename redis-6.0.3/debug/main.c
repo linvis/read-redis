2468e8ce1dcbf455d789c89de095cc0f05c9ac92
shlinym <jarvisfironman@gmail.com>
2020-05-28 20:27:47 +0800

    update

--- redis-6.0.3/debug/main.c
+++ redis-6.0.3/debug/main.c
@@ -22,8 +22,14 @@ int main(void)
 // zskiplist
 #if 1
     zskiplist *z1 = zslCreate();
-    sds s1 = sdsnewlen("hello", 5);
-    zslInsert(z1, 1.0, s1);
+    sds s1 = sdsnewlen("hello1", 5);
+    sds s2 = sdsnewlen("hello2", 5);
+    sds s3 = sdsnewlen("hello3", 5);
+    sds s4 = sdsnewlen("hello4", 5);
+    zslInsert(z1, 3.0, s1);
+    zslInsert(z1, 2.0, s2);
+    zslInsert(z1, 1.0, s3);
+    zslInsert(z1, 4.0, s4);
 #endif
 
     return 0;
